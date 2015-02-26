e57ee86a70b9737938383d3249ab23865ca84fb3
CaseyKelly <casey.kelly@colorado.edu>
2015-02-25 17:59:29 -0700

    test_name_sticks passes

--- ruby/robot-name/robot.rb
+++ ruby/robot-name/robot.rb
@@ -1,12 +1,15 @@
 class Robot
 
+  def initialize
+    @name = ''
+  end
+
   def name
     numbers = '1234567890'
     letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-    name = ''
-    2.times {|i| name << letters[rand(26)]}
-    3.times {|i| name << numbers[rand(10)]}
-    name
+    2.times {|i| @name << letters[rand(26)]}
+    3.times {|i| @name << numbers[rand(10)]}
+    @name
   end
 
 end
