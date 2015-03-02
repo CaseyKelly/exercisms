adfe15313a6a7aaf255c84113e8c6bdb5247e86e
CaseyKelly <casey.kelly@colorado.edu>
2015-03-01 18:48:51 -0700

    Through word-count

--- ruby/difference-of-squares/squares.rb
+++ ruby/difference-of-squares/squares.rb
@@ -6,4 +6,13 @@ class Squares
   def square_of_sums
     (1..@n).reduce(&:+) ** 2
   end
+
+  def sum_of_squares
+    (1..@n).reduce { |acc, n| acc += n ** 2}
+  end
+
+  def difference
+    square_of_sums - sum_of_squares
+  end
+
 end
