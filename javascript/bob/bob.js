045b17c2c7b6b771130593ba5a2c10f4421ea1a6
CaseyKelly <casey.kelly@colorado.edu>
2017-02-03 19:33:48 -0500

    all tests pass after trim whitepsace from input

--- javascript/bob/bob.js
+++ javascript/bob/bob.js
@@ -6,7 +6,7 @@
 var Bob = function() {};
 
 Bob.prototype.hey = function(input) {
-  if (input === "") {
+  if (input.trim() === "") {
     return "Fine. Be that way!"
   } else if (input === input.toUpperCase() && /[a-zA-Z]/.test(input)) {
     return "Whoa, chill out!"
