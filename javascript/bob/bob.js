c6ad488aafff722de4a7beed63f2f6aec9d46019
CaseyKelly <casey.kelly@colorado.edu>
2017-02-03 19:30:09 -0500

    test 16 passes after check for empty string input

--- javascript/bob/bob.js
+++ javascript/bob/bob.js
@@ -6,8 +6,10 @@
 var Bob = function() {};
 
 Bob.prototype.hey = function(input) {
-  if (input === input.toUpperCase() && /[a-zA-Z]/.test(input)) {
-    return 'Whoa, chill out!'
+  if (input === "") {
+    return "Fine. Be that way!"
+  } else if (input === input.toUpperCase() && /[a-zA-Z]/.test(input)) {
+    return "Whoa, chill out!"
   } else if (input.charAt(input.length - 1) === "?") {
     return "Sure."
   } else {
