558fd65899e3df537788618a0a4e6ec093eca44c
CaseyKelly <casey.kelly@colorado.edu>
2017-02-03 19:10:04 -0500

    test 8 passes

--- javascript/bob/bob.js
+++ javascript/bob/bob.js
@@ -6,7 +6,7 @@
 var Bob = function() {};
 
 Bob.prototype.hey = function(input) {
-  if (input === input.toUpperCase()) {
+  if (input === input.toUpperCase() && /[a-zA-Z]/.test(input)) {
     return 'Whoa, chill out!'
   } else if (input.charAt(input.length - 1) === "?") {
     return "Sure."
