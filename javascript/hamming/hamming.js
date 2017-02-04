6768d2a76efc84a0577c0b31e6c26bbe54109ac3
CaseyKelly <casey.kelly@colorado.edu>
2017-02-03 22:42:39 -0500

    all tests pass after add exception for strands of unequal length

--- javascript/hamming/hamming.js
+++ javascript/hamming/hamming.js
@@ -5,6 +5,10 @@ Hamming.prototype.compute = function(strandA, strandB) {
   var nucleotidesB = strandB.split("");
   var distance = 0;
 
+  if (nucleotidesA.length !== nucleotidesB.length) {
+    throw "DNA strands must be of equal length."
+  }
+
   for (var i = 0; i < nucleotidesA.length; i++) {
     if (nucleotidesA[i] !== nucleotidesB[i]) {
       distance += 1
