3cb896ef3bd3ddfc1ce795abed7bb3de9fcecb39
CaseyKelly <casey.kelly@colorado.edu>
2017-02-03 23:39:23 -0500

    pass 3 tests / create findComplement function to swap letters during iteration

--- javascript/rna-transcription/rna-transcription.js
+++ javascript/rna-transcription/rna-transcription.js
@@ -1,11 +1,21 @@
 var DnaTranscriber = function() {};
 
 DnaTranscriber.prototype.toRna = function(dnaStrand) {
-  if (dnaStrand === "C") {
-    return "G"
-  } else {
-    return "C"
+  var nucleotides = dnaStrand.split("");
+  var findComplement = function(nucleotide) {
+    if (nucleotide === "G") {
+      return "C"
+    } else if (nucleotide === "C") {
+      return "G"
+    } else if (nucleotide === "A") {
+      return "U"
+    }
+  };
+
+  for (var i = 0; i < nucleotides.length; i++) {
+    nucleotides[i] = findComplement(nucleotides[i])
   }
+  return nucleotides.join()
 };
 
 module.exports = DnaTranscriber;
