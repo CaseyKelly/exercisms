ca5578598208ec215133643ea1e5c84bd0056cd3
CaseyKelly <casey.kelly@colorado.edu>
2017-02-04 00:26:11 -0500

    refactor findComplement logic

--- javascript/rna-transcription/rna-transcription.js
+++ javascript/rna-transcription/rna-transcription.js
@@ -3,15 +3,16 @@ var DnaTranscriber = function() {};
 DnaTranscriber.prototype.toRna = function(dnaStrand) {
   var nucleotides = dnaStrand.split("");
 
+  var rnaKey = {
+    G: "C",
+    C: "G",
+    A: "U",
+    T: "A"
+  };
+
   var findComplement = function(nucleotide) {
-    if (nucleotide === "G") {
-      return "C"
-    } else if (nucleotide === "C") {
-      return "G"
-    } else if (nucleotide === "A") {
-      return "U"
-    } else if (nucleotide === "T") {
-      return "A"
+    if (rnaKey[nucleotide]) {
+      return rnaKey[nucleotide]
     } else {
       throw "Invalid input"
     }
