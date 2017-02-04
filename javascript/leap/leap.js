766ccff783d0aee5318191f72b2364fae497c2ff
CaseyKelly <casey.kelly@colorado.edu>
2017-02-03 21:33:07 -0500

    test 4 pass / check if year divisible by 400

--- javascript/leap/leap.js
+++ javascript/leap/leap.js
@@ -3,7 +3,9 @@ var Year = function(input) {
 };
 
 Year.prototype.isLeap = function(input) {
-  if ( this.year % 100 === 0 ) {
+  if ( this.year % 400 === 0 ) {
+    return true
+  } else if ( this.year % 100 === 0 ) {
     return false
   } else if ( this.year % 4 === 0 ) {
     return true
