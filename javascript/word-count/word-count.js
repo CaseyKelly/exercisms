fdaee9adde9fcc2c02fbc0cd16d982e8f6da5b6d
CaseyKelly <casey.kelly@colorado.edu>
2017-02-06 21:07:34 -0500

    returns object with keys of words and values of 1

--- javascript/word-count/word-count.js
+++ javascript/word-count/word-count.js
@@ -1,9 +1,13 @@
 var Words = function() {};
 
-Words.prototype.count = function() {
-    return {
-        word: 1
+Words.prototype.count = function(wordString) {
+    var wordArray = wordString.split(" ");
+    var wordsObject = {};
+
+    for (var i = 0; i < wordArray.length; i++) {
+      wordsObject[wordArray[i]] = 1;
     }
+    return wordsObject
 };
 
 module.exports = Words;
