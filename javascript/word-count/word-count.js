959af1b7731bb671a37b075a656630a0b830b444
CaseyKelly <casey.kelly@colorado.edu>
2017-02-06 23:32:27 -0500

    clean up

--- javascript/word-count/word-count.js
+++ javascript/word-count/word-count.js
@@ -4,16 +4,16 @@ Words.prototype.count = function(inputString) {
     var cleanString = inputString.replace(/[|&;$%@"<>()+:!^.?¡¿]/g, "").replace(/[\n\t]/g, " ");
     var wordArray = cleanString.split(/[ ,]+/).filter(word => word.trim() != '');
 
-    // console.log(wordArray);
-
     var wordsObject = {};
-    for (var i = 0; i < wordArray.length; i++) {
-      var word = wordArray[i].toLowerCase();
-      var chars = word.length;
-      if (word.charAt(0) === "\'") word = word.substring(1, chars--);
-	    if (word.charAt(--chars) === "\'") word = word.substring(0, chars);
-      wordsObject[word] = wordsObject[word] >= 1 ? wordsObject[word] + 1 : 1;
 
+    for (var i = 0; i < wordArray.length; i++) {
+        var word = wordArray[i].toLowerCase();
+        var chars = word.length;
+
+        if (word.charAt(0) === "\'") word = word.substring(1, chars--);
+        if (word.charAt(--chars) === "\'") word = word.substring(0, chars);
+        
+        wordsObject[word] = wordsObject[word] >= 1 ? wordsObject[word] + 1 : 1;
     }
     return wordsObject
 };
