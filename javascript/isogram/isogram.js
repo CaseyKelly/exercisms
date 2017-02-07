6fafc7c59d6482086777e77510583892153e3da2
CaseyKelly <casey.kelly@colorado.edu>
2017-02-06 23:59:39 -0500

    check for extra whitespace in word

--- javascript/isogram/isogram.js
+++ javascript/isogram/isogram.js
@@ -1,9 +1,8 @@
 var Isogram = function(word) {
-    this.word = word.toLowerCase().replace(/-/g, "");
+    this.word = word.toLowerCase().replace(/[- ]/g, "");
 };
 
 Isogram.prototype.isIsogram = function() {
-  console.log(this.word);
     var letters = this.word.split("")
     return letters.length === new Set(letters).size;
 };
