8186e4288381154fc2130ba2ec107451ffc18225
CaseyKelly <casey.kelly@colorado.edu>
2017-02-06 23:55:45 -0500

    now accounts for hyphens acting as non-unique set members

--- javascript/isogram/isogram.js
+++ javascript/isogram/isogram.js
@@ -1,8 +1,9 @@
 var Isogram = function(word) {
-    this.word = word.toLowerCase();
+    this.word = word.toLowerCase().replace(/-/g, "");
 };
 
 Isogram.prototype.isIsogram = function() {
+  console.log(this.word);
     var letters = this.word.split("")
     return letters.length === new Set(letters).size;
 };
