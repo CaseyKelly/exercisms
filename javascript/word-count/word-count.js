1ab731e3d48eb1ffc1d94f5916a62f36e69f7f1a
CaseyKelly <casey.kelly@colorado.edu>
2017-02-06 21:29:49 -0500

    loop and set keys from array while also sett value at 1 unless seen before

--- javascript/word-count/word-count.js
+++ javascript/word-count/word-count.js
@@ -1,11 +1,12 @@
 var Words = function() {};
 
 Words.prototype.count = function(wordString) {
-    var wordArray = wordString.split(" ");
+    var wordArray = wordString.split(" ")
     var wordsObject = {};
 
     for (var i = 0; i < wordArray.length; i++) {
-      wordsObject[wordArray[i]] = 1;
+      var num = wordArray[i];
+      wordsObject[num] = wordsObject[num] ? wordsObject[num] + 1 : 1;
     }
     return wordsObject
 };
