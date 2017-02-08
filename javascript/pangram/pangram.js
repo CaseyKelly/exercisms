1d50aee27311ab1386deec748717b5c1461809bc
CaseyKelly <casey.kelly@colorado.edu>
2017-02-07 21:31:11 -0500

    able to take string characters and iterate over alphabet array, removing each letter from array as we go

--- javascript/pangram/pangram.js
+++ javascript/pangram/pangram.js
@@ -1,6 +1,27 @@
-var Pangram = function() {};
+var Pangram = function(phrase) {
+    this.phrase = phrase.replace(/ /g, "");
+};
 
 Pangram.prototype.isPangram = function() {
+    var alphabet = ["a", "b", "c", "d", "e", "f", "g",
+                    "h", "i", "j", "k", "l", "m", "n",
+                    "o", "p", "q", "r", "s", "t", "u",
+                    "v", "w", "x", "y", "z"]
+    var index = 0;
+
+    var splitPhrase = this.phrase.split("")
+    if (splitPhrase.length < 26) {
+      return false
+    }
+    for (var i = 0; i < splitPhrase.length; i++) {
+      index = alphabet.indexOf(splitPhrase[i]);
+      if (index != -1) {
+        alphabet.splice(index, 1)
+      }
+      if (alphabet.length === 0) {
+        return true
+      }
+    }
     return false
 }
 
