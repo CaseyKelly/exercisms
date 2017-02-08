bf46b49d5c2a9c0596967c5000f560632c5bee1b
CaseyKelly <casey.kelly@colorado.edu>
2017-02-07 22:18:20 -0500

    accounts for mixed case and puctuation

--- javascript/pangram/pangram.js
+++ javascript/pangram/pangram.js
@@ -1,26 +1,28 @@
 var Pangram = function(phrase) {
-    this.phrase = phrase.replace(/ /g, "");
+    this.phrase = phrase.replace(/[" .]/g, "").toLowerCase();
 };
 
 Pangram.prototype.isPangram = function() {
     var alphabet = ["a", "b", "c", "d", "e", "f", "g",
-                    "h", "i", "j", "k", "l", "m", "n",
-                    "o", "p", "q", "r", "s", "t", "u",
-                    "v", "w", "x", "y", "z"]
+        "h", "i", "j", "k", "l", "m", "n",
+        "o", "p", "q", "r", "s", "t", "u",
+        "v", "w", "x", "y", "z"
+    ]
     var index = 0;
-
     var splitPhrase = this.phrase.split("")
+
+
     if (splitPhrase.length < 26) {
-      return false
+        return false
     }
     for (var i = 0; i < splitPhrase.length; i++) {
-      index = alphabet.indexOf(splitPhrase[i]);
-      if (index != -1) {
-        alphabet.splice(index, 1)
-      }
-      if (alphabet.length === 0) {
-        return true
-      }
+        index = alphabet.indexOf(splitPhrase[i]);
+        if (index != -1) {
+            alphabet.splice(index, 1)
+        }
+        if (alphabet.length === 0) {
+            return true
+        }
     }
     return false
 }
