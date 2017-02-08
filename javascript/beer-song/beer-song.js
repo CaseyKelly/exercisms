a2cd192369db4abd1d8327e2f3b0584f95cd919d
CaseyKelly <casey.kelly@colorado.edu>
2017-02-07 23:52:32 -0500

    returns different templates for verses one and two

--- javascript/beer-song/beer-song.js
+++ javascript/beer-song/beer-song.js
@@ -1,13 +1,20 @@
 var BeerSong = function() {};
 
 BeerSong.prototype.verse = function(verseNumber) {
+  var bottles1 = verseNumber + " bottles"
+  var bottles2 = (verseNumber - 1) + " bottles";
+  var one = "one"
+
   if (verseNumber === 2){
-    var verseTemplate = verseNumber.toString() + " bottles of beer on the wall, " + verseNumber.toString() + " bottles of beer.\n" +
-    "Take one down and pass it around, " + (verseNumber - 1).toString() + " bottle of beer on the wall.\n"
-  } else {
-    var verseTemplate = verseNumber.toString() + " bottles of beer on the wall, " + verseNumber.toString() + " bottles of beer.\n" +
-    "Take one down and pass it around, " + (verseNumber - 1).toString() + " bottles of beer on the wall.\n"
+    bottles2 = bottles2.slice(0, -1);
+  } else if (verseNumber === 1) {
+    bottles1 = bottles1.slice(0, -1);
+    bottles2 = "no more bottles"
+    one = "it"
   }
+
+  var verseTemplate = bottles1 + " of beer on the wall, " + bottles1 + " of beer.\n" +
+  "Take " + one + " down and pass it around, " + bottles2 + " of beer on the wall.\n"
 
   return verseTemplate
 };
