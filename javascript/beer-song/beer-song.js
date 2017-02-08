69d7c4382a9e6952bc51fc6ff350f3054fdd78c3
CaseyKelly <casey.kelly@colorado.edu>
2017-02-08 00:21:31 -0500

    add sing funtion to returnmultiple verses

--- javascript/beer-song/beer-song.js
+++ javascript/beer-song/beer-song.js
@@ -1,22 +1,22 @@
 var BeerSong = function() {};
 
 BeerSong.prototype.verse = function(verseNumber) {
-  var bottles1 = verseNumber + " bottles"
-  var bottles2 = (verseNumber - 1) + " bottles";
-  var one = "one"
-
   if (verseNumber === 2){
-    bottles2 = bottles2.slice(0, -1);
+    return '2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n'
   } else if (verseNumber === 1) {
-    bottles1 = bottles1.slice(0, -1);
-    bottles2 = "no more bottles"
-    one = "it"
+    return '1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n'
+  } else if (verseNumber === 0) {
+    return 'No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n'
   }
+  return verseNumber + ' bottles of beer on the wall, ' + verseNumber + ' bottles of beer.\nTake one down and pass it around, ' + (verseNumber - 1) + ' bottles of beer on the wall.\n'
+};
 
-  var verseTemplate = bottles1 + " of beer on the wall, " + bottles1 + " of beer.\n" +
-  "Take " + one + " down and pass it around, " + bottles2 + " of beer on the wall.\n"
-
-  return verseTemplate
+BeerSong.prototype.sing = function(verseStart, verseEnd) {
+  var verses = ""
+  for (var i = verseStart; i >= verseEnd; i--) {
+    verses += this.verse(i) + "\n"
+  }
+  return verses.slice(0, -1)
 };
 
 module.exports = BeerSong;
