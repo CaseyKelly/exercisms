1a5bbb849a9f19f654f011a2f40277a7703f2c45
CaseyKelly <casey.kelly@colorado.edu>
2017-02-08 19:18:42 -0500

    beautify

--- javascript/phone-number/phone-number.js
+++ javascript/phone-number/phone-number.js
@@ -1,9 +1,9 @@
 var PhoneNumber = function(input) {
     this.input = input.replace(/[-)(. ]/g, "");
     if (this.input.length === 11 && input[0] === '1') {
-      this.input = this.input.slice(1, 11)
+        this.input = this.input.slice(1, 11)
     } else if (this.input.length != 10) {
-      this.input = '0000000000'
+        this.input = '0000000000'
     }
 };
 
@@ -16,7 +16,7 @@ PhoneNumber.prototype.areaCode = function() {
     return this.areaCode
 };
 
-PhoneNumber.prototype.toString = function () {
+PhoneNumber.prototype.toString = function() {
     this.areaCode = this.input[0] + this.input[1] + this.input[2];
     return "(" + this.areaCode + ") " + this.input.slice(3, 6) + "-" + this.input.slice(6, 10)
 };
