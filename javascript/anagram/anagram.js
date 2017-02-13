7345734722af7e09e8ac3b75cdae4ba8054b6398
Casey Kelly <casey.kelly@colorado.edu>
2017-02-13 18:40:04 -0500

    split, sort and join strings to see if they are equal

--- javascript/anagram/anagram.js
+++ javascript/anagram/anagram.js
@@ -1,12 +1,13 @@
 var Anagram = function(input) {
   this.input = input;
+  this.split = input.split("");
 };
 
 Anagram.prototype.matches = function (wordArray) {
   anagrams = [];
   
   for (var i = 0; i < wordArray.length; i++) {
-    if (this.input.length === wordArray[i].length) {
+    if (wordArray[i].split("").sort().join() === this.split.sort().join()) {
       anagrams.push(wordArray[i])
     }
   }
