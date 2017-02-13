974b8f62b0e7b2f6426491193927d7a15d0e8532
Casey Kelly <casey.kelly@colorado.edu>
2017-02-13 14:28:02 -0500

    anagrams are returned if string lengths are equivalent

--- javascript/anagram/anagram.js
+++ javascript/anagram/anagram.js
@@ -1,7 +1,16 @@
-var Anagram = function() {};
+var Anagram = function(input) {
+  this.input = input;
+};
 
-Anagram.prototype.matches = function () {
-  return [ ]
+Anagram.prototype.matches = function (wordArray) {
+  anagrams = [];
+  
+  for (var i = 0; i < wordArray.length; i++) {
+    if (this.input.length === wordArray[i].length) {
+      anagrams.push(wordArray[i])
+    }
+  }
+  return anagrams
 };
 
 module.exports = Anagram;
