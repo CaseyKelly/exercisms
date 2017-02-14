636dd74f9c9e8a6c6068aa8e6a83cf4ce1b20ef2
Casey Kelly <casey.kelly@colorado.edu>
2017-02-13 20:19:28 -0500

    first test passes with return of first line

---
+++ javascript/food-chain/food-chain.js
@@ -0,0 +1,7 @@
+var FoodChain = function() {};
+
+FoodChain.prototype.verse = function () {
+  return 'I know an old lady who swallowed a fly.\nI don\'t know why she swallowed the fly. Perhaps she\'ll die.\n'
+};
+
+module.exports = FoodChain;
