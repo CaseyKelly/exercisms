2ab4985338559c048023881e6b631977b05e393f
Casey Kelly <casey.kelly@colorado.edu>
2017-02-20 10:00:31 -0500

    verse() prints two verses

--- javascript/food-chain/food-chain.js
+++ javascript/food-chain/food-chain.js
@@ -1,7 +1,14 @@
 var FoodChain = function() {};
 
-FoodChain.prototype.verse = function () {
-  return 'I know an old lady who swallowed a fly.\nI don\'t know why she swallowed the fly. Perhaps she\'ll die.\n'
+FoodChain.prototype.verse = function(verseNum) {
+    var refrain = 'I don\'t know why she swallowed the fly. Perhaps she\'ll die.\n'
+  
+    if (verseNum === 2) {
+      refrain = 'I know an old lady who swallowed a spider.\nIt wriggled and jiggled and tickled inside her.\nShe swallowed the spider to catch the fly.\n' + refrain
+    } else if (verseNum === 1) {
+      refrain  = 'I know an old lady who swallowed a fly.\n' + refrain
+    }
+      return refrain
 };
 
 module.exports = FoodChain;
